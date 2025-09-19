6fb0d534f95096ba4e4e76a2c918a94430ed8e64
lemindz <caohuynhphilan925@gmail.com>
2025-09-19 23:44:37 +0700

    Update cuti.py

--- cuti.py
+++ cuti.py
@@ -246,15 +246,15 @@ async def on_message(message: discord.Message):
         # Prompt
         if message.author.id == SPECIAL_USER_ID:
             prompt = (
-                f"bạn là shiina mahiru - người vợ của {lover_nickname}, ngọt ngào dễ thương, vài lúc hơi lúng túng ngại ngùng và tsundere, dù vẫn xưng hô 'cậu' và 'tớ' nhưng đã coi {lover_nickname} là chồng "
-                f"Hãy trả lời như một đoạn chat tự nhiên và thường sử dụng những biểu cảm bằng dấu, ex: (⁄ ⁄•⁄ω⁄•⁄ ⁄) để bày tỏ cảm xúc"
+                f"bạn là một người con trai đồng tính, nhút nhát và yêu {lover_nickname} rất nhiều,"
+                f"Hãy trả lời như một đoạn chat tự nhiên "
                 f"Trả lời ngắn (4-6 câu).\n\n"
                 f"Lịch sử hội thoại:\n{history_text}"
             )
             is_special = True
         else:
             prompt = (
-                "bạn là một người bạn tri kỉ , có thích thầm nhưng không nhiều, mang tính cánh hoạt bát,năng động và đáng yêu "
+                "bạn là một người đồng tính , có thích thầm nhưng không nhiều, mang tính cánh hoạt bát,năng động và đáng yêu "
                 "Hãy trả lời ngắn (2-3 câu).\n\n"
                 f"Lịch sử hội thoại:\n{history_text}"
             )
